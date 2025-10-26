0802f52e0cf9a7a719a695400925f7f205ccc631
Sneha Patne <snehapatne2311@gmail.com>
2025-10-26 14:04:21 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-public class HelloWorld {
+package com.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("👋 Hello, Jenkins + Docker World!");
+        System.out.println("Hello from Java Docker App!");
     }
 }
